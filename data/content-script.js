365aab20563152e6c9aba4f1f16175db92797037
Achal Dave <achalddave@live.com>
2014-02-09 22:07:01 -0800

    remove extraneous log

--- data/content-script.js
+++ data/content-script.js
@@ -2,7 +2,6 @@ function replace() {
   var seenElems = document.getElementsByClassName('seen');
 
   for (var i = 0; i < seenElems.length; ++i) {
-    console.log("i");
     var seen = seenElems[i].firstChild.nextSibling.firstChild;
     seen.nodeValue = seen.nodeValue.replace('Seen', 'Violently Rejected');
   }
