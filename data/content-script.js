69da617d4908f089328b67ad08b296bbcdd2dadd
Achal Dave <achalddave@live.com>
2014-02-11 22:11:06 -0800

    lowercase rejected

--- data/content-script.js
+++ data/content-script.js
@@ -3,7 +3,7 @@ function replace() {
 
   for (var i = 0; i < seenElems.length; ++i) {
     var seen = seenElems[i].firstChild.nextSibling.firstChild;
-    seen.nodeValue = seen.nodeValue.replace('Seen', 'Violently Rejected');
+    seen.nodeValue = seen.nodeValue.replace('Seen', 'Violently rejected');
   }
 }
 
